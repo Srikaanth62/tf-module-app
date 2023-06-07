1008f613c7107e3b2eeee85468bf513518ec6d39
srikaanth <srikaanth62@gmail.com>
2023-06-07 12:50:30 +0530

    terraform module

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "env" {}
+variable "name" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "bastion_cidr" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_capacity" {}
+
